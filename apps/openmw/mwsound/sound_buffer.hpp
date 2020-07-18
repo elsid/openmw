55fa758ca9d9c498ccd3355c76169a76acdbe29e
elsid <elsid.mail@gmail.com>
2020-07-18 23:56:14 +0200

    Separate sound buffer pool from sound manager

--- apps/openmw/mwsound/sound_buffer.hpp
+++ apps/openmw/mwsound/sound_buffer.hpp
@@ -1,27 +1,109 @@
 #ifndef GAME_SOUND_SOUND_BUFFER_H
 #define GAME_SOUND_SOUND_BUFFER_H
 
+#include <algorithm>
 #include <string>
+#include <memory>
+#include <deque>
+#include <unordered_map>
 
 #include "sound_output.hpp"
 
+#include <components/misc/objectpool.hpp>
+
+namespace ESM
+{
+    struct Sound;
+}
+
+namespace VFS
+{
+    class Manager;
+}
+
 namespace MWSound
 {
+    struct SoundBufferParams
+    {
+        std::string mResourceName;
+        float mVolume = 0;
+        float mMinDist = 0;
+        float mMaxDist = 0;
+    };
+
+    class SoundBufferPool;
+
     class Sound_Buffer
     {
-    public:
-        std::string mResourceName;
+        public:
+            void init(SoundBufferParams params)
+            {
+                mParams = std::move(params);
+                mHandle = nullptr;
+                mUses = 0;
+            }
 
-        float mVolume;
-        float mMinDist, mMaxDist;
+            const std::string& getResourceName() const noexcept { return mParams.mResourceName; }
 
-        Sound_Handle mHandle;
+            Sound_Handle getHandle() const noexcept { return mHandle; }
 
-        size_t mUses;
+            float getVolume() const noexcept { return mParams.mVolume; }
 
-        Sound_Buffer(std::string resname, float volume, float mindist, float maxdist)
-          : mResourceName(resname), mVolume(volume), mMinDist(mindist), mMaxDist(maxdist), mHandle(nullptr), mUses(0)
-        { }
+            float getMinDist() const noexcept { return mParams.mMinDist; }
+
+            float getMaxDist() const noexcept { return mParams.mMaxDist; }
+
+        private:
+            SoundBufferParams mParams;
+            Sound_Handle mHandle = nullptr;
+            std::size_t mUses = 0;
+
+            friend class SoundBufferPool;
+    };
+
+    class SoundBufferPool
+    {
+        public:
+            SoundBufferPool(const VFS::Manager& vfs, Sound_Output& output);
+
+            ~SoundBufferPool();
+
+            Sound_Buffer* lookup(const std::string& soundId) const;
+
+            Sound_Buffer* load(const std::string& soundId);
+
+            void use(Sound_Buffer& sfx) noexcept
+            {
+                if (sfx.mUses++ == 0)
+                {
+                    const auto it = std::find(mUnusedBuffers.begin(), mUnusedBuffers.end(), &sfx);
+                    if (it != mUnusedBuffers.end())
+                        mUnusedBuffers.erase(it);
+                }
+            }
+
+            void release(Sound_Buffer& sfx)
+            {
+                if (--sfx.mUses == 0)
+                    mUnusedBuffers.push_front(&sfx);
+            }
+
+            void clear();
+
+        private:
+            const VFS::Manager* const mVfs;
+            Sound_Output* mOutput;
+            Misc::ObjectPool<Sound_Buffer> mSoundBuffers;
+            std::unordered_map<std::string, Misc::ObjectPtr<Sound_Buffer>> mBufferNameMap;
+            std::size_t mBufferCacheMin;
+            std::size_t mBufferCacheMax;
+            std::size_t mBufferCacheSize = 0;
+            // NOTE: unused buffers are stored in front-newest order.
+            std::deque<Sound_Buffer*> mUnusedBuffers;
+
+            inline Sound_Buffer* insertSound(const std::string& soundId, const ESM::Sound& sound);
+
+            inline void unloadUnused();
     };
 }
 
